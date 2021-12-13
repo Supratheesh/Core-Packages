4c0939b228b3731ac2d5a5ba20b59c7d2d3703f7
Supratheesh Surendran <supratheesh.surendran@hrblock.com>
2021-12-13 20:19:05 +0530

    first commit

---
+++ MyProduct.API.Extensions/ContollerBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyProduct.API.Extensions
+{
+    public abstract class ContollerBase: ControllerBase
+    {
+    }
+}
