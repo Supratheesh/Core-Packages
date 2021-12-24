b0abb2cbe6e9d52e2e9cf15ceaba17274036b7b3
Supratheesh Surendran <supratheesh.surendran@hrblock.com>
2021-12-24 11:44:45 +0530

    Removed default constructor

--- HttpClient.Extensions/MyProduct.HttpClientExtensions.Consumer/Controllers/WeatherForecastController.cs
+++ HttpClient.Extensions/MyProduct.HttpClientExtensions.Consumer/Controllers/WeatherForecastController.cs
@@ -11,10 +11,7 @@ namespace MyProduct.HttpClientExtensions.Consumer.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        public WeatherForecastController()
-        {
-
-        }
+     
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
